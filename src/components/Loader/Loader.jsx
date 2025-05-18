3d8985e78026eda3c1f2156fed36e5d8f417d7a6
Sergii Klimchuk <v.buh911@gmail.com>
2025-05-18 13:22:10 +0300

    Initial commit

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import { ClipLoader } from 'react-spinners';
+import styles from './Loader.module.css';
+
+export function Loader() {
+  return (
+    <div className={styles.loader}>
+      <ClipLoader size={50} color="#007bff" />
+    </div>
+  );
+}
+
+
